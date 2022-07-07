009dd46ee0b0f5f56acd4703eaff52a227b5adb3
Harborth, Allen <allenharborth9835@gmail.com>
2022-07-06 20:41:00 -0500

    init commit

---
+++ express-essentials/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import data from './data/mock.json' assert {type: "json"};
+
+const app = express();
+
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+  console.log(`The server is running on port ${PORT}!`);
+  console.log(data);
+})
